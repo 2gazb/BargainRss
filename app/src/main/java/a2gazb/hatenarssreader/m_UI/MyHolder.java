0aa5d56250dfa88cac576af6591d5dbec51928be
2b <nagisa1980@gmail.com>
2018-08-30 23:52:39 +0900

    first_commit

---
+++ app/src/main/java/a2gazb/hatenarssreader/m_UI/MyHolder.java
@@ -0,0 +1,27 @@
+package a2gazb.hatenarssreader.m_UI;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import a2gazb.hatenarssreader.R;
+
+/**
+ * Created by Oclemy on 6/11/2016 for ProgrammingWizards Channel and http://www.camposha.com.
+ */
+public class MyHolder extends RecyclerView.ViewHolder {
+
+    TextView titleTxt,descTxt,dateTxt;
+    ImageView thumImg;
+
+    public MyHolder(View itemView) {
+        super(itemView);
+
+        titleTxt= (TextView) itemView.findViewById(R.id.titleTxt);
+        descTxt= (TextView) itemView.findViewById(R.id.descTxt);
+        dateTxt= (TextView) itemView.findViewById(R.id.dateTxt);
+        thumImg= (ImageView) itemView.findViewById(R.id.thumImg);
+
+    }
+}
